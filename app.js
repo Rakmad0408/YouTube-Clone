3c98f95b6f63ea5666669e34e39cf0d6cb27c8b0
Rakesh Saranathu <160518488+Rakmad0408@users.noreply.github.com>
2025-02-21 15:15:50 +0530

    Added files

---
+++ app.js
@@ -0,0 +1,133 @@
+const obj = [{
+  link: 'https://www.youtube.com/watch?v=h2JH0vqDcYc',
+  thumbnail: 'https://i.ytimg.com/vi/h2JH0vqDcYc/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBbt_YOZ-FMr7GgUs0_lMn3GI8QNA',
+  videoTime: '5:13',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Baby Won\'t You Tell Me Video Song | Saaho Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '150M views &#183; 5years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=Jz4fvd7ftS0',
+  thumbnail: 'https://i.ytimg.com/vi/fz9IDb2FV-0/maxresdefault.jpg',
+  videoTime: '3:07',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Padi Padi Leche Video Song | Padi Padi Leche Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '19M views &#183; 4 years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=IJq0yyWug1k&pp=ygUUdHVtIGhpIGhvIHZpZGVvIHNvbmc%3D',
+  thumbnail: 'https://i.ytimg.com/vi/NUo8CKI34o4/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBuQfEocHSU8Qc5LXBzKRbNFaiaFQ',
+  videoTime: '5:10',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Tum Hi Ho Video Song | Aashiqui 2 Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '143M views &#183; 23 years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=TOixmcp3B_Q',
+  thumbnail: 'https://tse3.mm.bing.net/th?id=OIP.yFNLQh_d6YfH9YvogoZmLAHaEK&pid=Api&P=0&h=180',
+  videoTime: '5:44',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Unnatundi Gundey Video Song | Ninnu Kori Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '18M views &#183; 7 years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=VAdGW7QDJiU&pp=ygUMY2hhbGV5YSBzb25n',
+  thumbnail: 'https://i.ytimg.com/vi/VAdGW7QDJiU/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA54A-0CogODxfMVFeTem1TSpTX3Q',
+  videoTime: '3:08',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Chaleya Video Song | Jawaan Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '100M views &#183; 2 years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=yEBee5d_S8U',
+  thumbnail: 'https://tse4.mm.bing.net/th?id=OIP.IJy1xmDpDksn92v73bSIbwHaEK&pid=Api&P=0&h=180',
+  videoTime: '4:24',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Ammaye Sannaga Video Song | Kushi Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '500M views &#183; 10 years ago'
+},{
+  link: 'https://www.youtube.com/watch?v=2Uc9Pw9uS9k',
+  thumbnail: 'https://i.ytimg.com/vi/2Uc9Pw9uS9k/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCYeBdsOcAyq-pgIknJDfLdhsx1WA',
+  videoTime: '3:42',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Pilla Nuvvuleni Jevitam Video Song | Gabbar Singh Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '121M views &#183; 1 year ago'
+},{
+  link: 'https://www.youtube.com/watch?v=hVCYwwFwGEE',
+  thumbnail: 'https://i.ytimg.com/vi/TbkJ5HhFXdg/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLAQqn4vR3miWIGsCqlxw2cPvLEvRA',
+  videoTime: '3:51',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Duniya Video Song | Luka Chupi Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '56M views &#183; 72 days ago'
+},{
+  link: 'https://www.youtube.com/watch?v=A56mZxSXKn8&pp=ygUQaSB3YW5uYSBmbHkgc29uZw%3D%3D',
+  thumbnail: 'https://i.ytimg.com/vi/A56mZxSXKn8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCXKIyub2RqnofQhno7q7WCZ7Dj7g',
+  videoTime: '4:40',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'I Wanna Fly Video Song | Krishnarjuna Yuddham Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '100M views &#183; 14 days ago'
+},{
+  link: 'https://www.youtube.com/watch?v=ElZfdU54Cp8&pp=ygUWYXBuYSBiYW5hIGxlIHBpeWEgc29uZw%3D%3D',
+  thumbnail: 'https://i.ytimg.com/vi/ElZfdU54Cp8/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLD86pry-dWVXJt4ZCScDJCOGpS37A',
+  videoTime: '3:25',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Apna Bana Le Video Song | Bhediya Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '96M views &#183; 30 days ago'
+},{
+  link: 'https://www.youtube.com/watch?v=Pal0uollc4E&pp=ygURbWFuYXN1IG1hcnJlIHNvbmc%3D',
+  thumbnail: 'https://i.ytimg.com/vi/Pal0uollc4E/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLDn56VdK4C9QxqS1VoUq7nF1SP0ag',
+  videoTime: '4:27',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Manasu Maree Video Song | V Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '29M views &#183; 46 days ago'
+},{
+  link: 'https://www.youtube.com/watch?v=QonKHyRd7kU',
+  thumbnail: 'https://i.ytimg.com/vi/QonKHyRd7kU/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLCUN9QjedgfLbA49lKKXajk2FOsTA',
+  videoTime: '19:59',
+  profilePicture: 'https://tse4.mm.bing.net/th?id=OIP.yDncA-XRfTzWHb8jrjyoiwHaHa&pid=Api&P=0&h=180',
+  videoTitle: 'Bapu Gari Bommo Video Song | Attarintiki Daredi Movie',
+  videoAurthor: 'Rakesh Music',
+  videoStats: '143M views &#183; 25 days ago'
+}];
+
+
+
+let html = '';
+
+
+
+obj.forEach(value => {
+  html+=`
+      <div class="video-preview">
+        <div class="thumbnail-row">
+          <a href="${value.link}">
+          <img class="thumbnail" src="${value.thumbnail}">
+          <div class="video-time">${value.videoTime}</div>
+          </a>
+        </div>
+        <div class="video-grid">
+          <div class="channel-picture">
+            <img class="profile-picture" src="${value.profilePicture}">
+          </div>
+          <div class="video-info">
+            <p class="video-title">
+              ${value.videoTitle}
+            </p>
+            <p class="video-aurthor">
+              ${value.videoAurthor}
+            </p>
+            <p class="video-stats">
+              ${value.videoStats}
+            </p>
+          </div>
+        </div>
+      </div>`
+});
+
+document.querySelector('.total-videos-grid').innerHTML = html;
